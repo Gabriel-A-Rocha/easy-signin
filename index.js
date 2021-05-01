f884f19dfe34eb5363ed2931b90406ae744a36ed
Gabriel Rocha <gabriel.altair.rocha@gmail.com>
2021-04-30 22:15:32 -0400

    chore: install dotenv

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 const express = require("express");
 const credentials = require("./credentials.json");
+const puppeteer = require("puppeteer");
 
-console.table(credentials.login);
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    isLandscape: true,
+    defaultViewport: null,
+  });
+  const page = await browser.newPage();
+
+  //await page.goto("https://dev.simplista4sobeys.com/");
+  await page.goto("https://github.com/login");
+
+  //await page.type("#login_field", process.env.GITHUB_USER);
+  //await page.type("#password", process.env.GITHUB_PWD);
+
+  //await browser.close();
+
+  console.log(process.env);
+})();
