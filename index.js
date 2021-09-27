0ce30232abbed840c064cb207bc5b2d689c0aa0c
Gabriel Rocha <gabriel.altair.rocha@gmail.com>
2021-09-27 12:06:57 -0400

    fix: export app functionality as a function

--- index.js
+++ index.js
@@ -1,39 +1,43 @@
-const express = require("express");
-const automatedLogin = require("./login/automatedLogin.js");
-const { join } = require("path");
+function esi() {
+  const express = require("express");
+  const automatedLogin = require("./login/automatedLogin.js");
+  const { join } = require("path");
 
-const signin = require("./credentials/credentials.json");
+  const signin = require("./credentials/credentials.json");
 
-const open = require("open");
+  const open = require("open");
 
-const app = express();
+  const app = express();
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static("login"));
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.static("login"));
 
-app.set("view engine", "ejs");
+  app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  const htmlPath = join(__dirname, "login", "login.ejs");
-  return res.render(htmlPath, { file: signin });
-});
+  app.get("/", (req, res) => {
+    const htmlPath = join(__dirname, "login", "login.ejs");
+    return res.render(htmlPath, { file: signin });
+  });
 
-app.post("/", (req, res) => {
-  const url = Object.keys(req.body)[0];
-  const role = Object.values(req.body)[0];
+  app.post("/", (req, res) => {
+    const url = Object.keys(req.body)[0];
+    const role = Object.values(req.body)[0];
 
-  const credentials = signin[url].find((l) => l.name === role);
-  const user = credentials.login;
-  const password = credentials.password;
+    const credentials = signin[url].find((l) => l.name === role);
+    const user = credentials.login;
+    const password = credentials.password;
 
-  automatedLogin(url, user, password);
+    automatedLogin(url, user, password);
 
-  return res.redirect("/");
-});
+    return res.redirect("/");
+  });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Login app running at ${port}`));
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => console.log(`Login app running at ${port}`));
 
-open(`http://localhost:${port}`, {
-  app: { name: open.apps.chrome, arguments: ["--incognito"] },
-});
+  open(`http://localhost:${port}`, {
+    app: { name: open.apps.chrome, arguments: ["--incognito"] },
+  });
+}
+
+module.exports = esi;
